4e18ffa469ce12cb0ee919c4277cb3627f7eb301
Vatar007 <tom.lenny.ruppert@gmail.com>
2026-05-01 10:57:37 +0200

    Main Menu Grundlage

--- src/ui/LoadGameButton.java
+++ src/ui/LoadGameButton.java
@@ -1,23 +1,53 @@
 package ui;
 
 import core.GameStarter;
-import entities.Player;
+import greenfoot.Greenfoot;
 import greenfoot.GreenfootImage;
+import greenfoot.MouseInfo;
 
 import java.util.List;
 
 public class LoadGameButton extends UI {
 
     private GameStarter gameStarter;
+    private  GreenfootImage loadGameButton = new GreenfootImage("Map/LoadGame.png");
+    private GreenfootImage loadGameButtonGlowing = new GreenfootImage("LoadGameGlowing.png");
+    private boolean isScaled = false;
 
     public LoadGameButton(GameStarter gameStarter){
         super();
         this.gameStarter = gameStarter;
         GreenfootImage LoadGameButton = new GreenfootImage("Map/LoadGame.png");
-        LoadGameButton.scale(310,240);
+        LoadGameButton.scale(240,110);
         setImage(LoadGameButton);
 
     }
+    public void act(){
+        mouseHover();
+
+    }
+    private void mouseHover(){
+        MouseInfo mouse = Greenfoot.getMouseInfo();
+        if (!isScaled) {
+            loadGameButton.scale(240, 110);
+            loadGameButtonGlowing.scale(240, 110);
+            isScaled = true;
+        }
+
+
+        if (mouse != null) {
+            setImage(loadGameButton);
+            List objects = getWorld().getObjectsAt(mouse.getX(), mouse.getY(), LoadGameButton.class);
+            for (Object object : objects)
+            {
+                if (object == this)
+                {
+                    setImage(loadGameButtonGlowing);
+                }
+            }
+        }
+    }
+
 
 
 
--- src/ui/StartButton.java
+++ src/ui/StartButton.java
@@ -3,20 +3,30 @@ package ui;
 import core.GameStarter;
 import greenfoot.*;
 
+import java.util.List;
+
 public class StartButton extends UI implements Clickable{
 
     private GameStarter gameStarter;
+    private GreenfootImage StartButton = new GreenfootImage("Map/StartGame.png");
+    private GreenfootImage StartButtonGlowing = new GreenfootImage("StartGameGlowing.png");
+    private boolean isScaled = false;
+
 
     public StartButton(GameStarter gameStarter){
         super();
         this.gameStarter = gameStarter;
         GreenfootImage StartButton = new GreenfootImage("Map/StartGame.png");
-        StartButton.scale(310,240);
+        GreenfootImage StartButtonGlowing = new GreenfootImage("StartGameGlowing.png");
+        StartButtonGlowing.scale(240,110);
+        StartButton.scale(240,110);
         setImage(StartButton);
     }
 
     @Override
     public void act() {
+
+
         if (Greenfoot.mouseClicked(null)) {
             MouseInfo mouse = Greenfoot.getMouseInfo();
             if (mouse != null) {
@@ -27,6 +37,7 @@ public class StartButton extends UI implements Clickable{
                 }
             }
         }
+        mouseHover();
     }
 
 
@@ -36,4 +47,29 @@ public class StartButton extends UI implements Clickable{
         gameStarter.start();
         return null;
     }
+
+
+    private void mouseHover(){
+        MouseInfo mouse = Greenfoot.getMouseInfo();
+        if (!isScaled) {
+            StartButton.scale(240, 110);
+            StartButtonGlowing.scale(240, 110);
+            isScaled = true;
+        }
+
+
+        if (mouse != null) {
+            setImage(StartButton);
+            List objects = getWorld().getObjectsAt(mouse.getX(), mouse.getY(), StartButton.class);
+            for (Object object : objects)
+            {
+                if (object == this)
+                {
+                    setImage(StartButtonGlowing);
+                }
+            }
+        }
+    }
 }
+
+
