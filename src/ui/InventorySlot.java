e764d6ee6b5e140335e3026ebfb5b466b9f38acc
Vatar007 <tom.lenny.ruppert@gmail.com>
2026-05-29 13:26:28 +0200

    Inventar grob fertiog zum testen Backpack inventar fertig und nutzbar im backpack fehlt noch die Rüstungsasicht und waffen

--- src/ui/InventorySlot.java
+++ src/ui/InventorySlot.java
@@ -2,38 +2,44 @@ package ui;
 
 import greenfoot.Actor;
 import greenfoot.GreenfootImage;
-import javafx.scene.image.Image;
 
 public class InventorySlot extends Actor {
     private Actor item;
+    private GreenfootImage baseImage; // Initialize in constructors
 
-
-     private GreenfootImage Image = new GreenfootImage("InventorySlot.png");
-
-
-
-
-
+    // Default constructor, calls the parameterized one with a default size
     public InventorySlot() {
+        this(200, 200); // Default size, assuming 200x200 fills a 2x2 cell area
+    }
 
-        GreenfootImage Image = new GreenfootImage("InventorySlot.png");
-        Image.scale(60,60);
-        setImage(Image);
+    // New constructor to specify slot pixel dimensions
+    public InventorySlot(int pixelWidth, int pixelHeight) {
+        baseImage = new GreenfootImage("InventorySlot.png");
+        baseImage.scale(pixelWidth, pixelHeight);
+        setImage(new GreenfootImage(baseImage));
     }
 
     public InventorySlot(Actor item) {
-        this();
+        this(); // Calls the default constructor, which in turn calls the parameterized one
         setItem(item);
     }
 
     public void setItem(Actor item) {
         this.item = item;
-        if (item == null) {
-            Image.scale(60,60);
-            setImage(new GreenfootImage(Image));
-        } else {
-            getImage().drawImage(new GreenfootImage(item.getImage()), 5, 5);
+        GreenfootImage currentImage = new GreenfootImage(baseImage); // Start with the scaled base image
+
+        if (item != null) {
+            // Draw the item's image onto the scaled base image
+            // Assuming item's image might be smaller, center it
+            GreenfootImage itemImage = item.getImage();
+            // Ensure itemImage is not null before drawing
+            if (itemImage != null) {
+                currentImage.drawImage(itemImage,
+                                       (currentImage.getWidth() - itemImage.getWidth()) / 2,
+                                       (currentImage.getHeight() - itemImage.getHeight()) / 2);
+            }
         }
+        setImage(currentImage);
     }
 
     public Actor getItem() {
