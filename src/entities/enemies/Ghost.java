d677f367e68e488626fc805fba93f7289b736e3f
NorkelL <n.l.lazic@outlook.com>
2026-06-12 11:17:00 +0200

    Add Ghost class extending BaseMonster

---
+++ src/entities/enemies/Ghost.java
@@ -0,0 +1,10 @@
+package entities.enemies;
+
+import entities.base.BaseMonster;
+import entities.util.Hitting;
+
+public class Ghost extends BaseMonster {
+public Ghost(int life){super (120,10,12);
+
+}
+}
