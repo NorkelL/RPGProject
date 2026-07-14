7835b73284aaab0d7a4bfc4057a829cf716fc587
NorkelL <n.l.lazic@outlook.com>
2026-07-14 15:33:17 +0200

    complete save and resume logic - no implimentation HealthPotion and useable fixes

--- src/items/armor/Armor.java
+++ src/items/armor/Armor.java
@@ -12,17 +12,9 @@ public class Armor extends Item {
     @OnHover.ShowOnHover
     private final String material; // "leather", "iron", etc.
 
-    @OnHover.ShowOnHover
-    public Rarity rarity;
-
     public Armor(String slotType, String material) {
-        this(slotType,material,Rarity.setRarity());
-    }
-
-    public Armor(String slotType, String material, Rarity rarity) {
         this.slotType = slotType;
         this.material = material;
-        this.rarity = rarity;
 
         // Setzt das Bild für das Item auf dem Boden oder im Slot
         // z.B. "items/leather_chest.png"
