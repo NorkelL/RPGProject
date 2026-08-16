e3f1c50fa6a17df182972c6fc305bbf2d2d704ba
FlyLennox <mlennox@outlook.de>
2026-08-16 21:29:37 +0200

    LevelCounter

---
+++ src/ui/LevelCounter.java
@@ -0,0 +1,14 @@
+package ui;
+import greenfoot.GreenfootImage;
+import util.FontManager;
+import java.awt.Color;
+public class LevelCounter extends UI{
+
+    public LevelCounter(int level) {
+        GreenfootImage text = FontManager.renderText("Level " + level, FontManager.getMinecraft(14f), new Color(74, 83, 112));
+        GreenfootImage background = new GreenfootImage("UI/MainMenu/InfoText.png");
+        background.scale(text.getWidth()+20,text.getHeight()+20);
+        background.drawImage(text,10,10);
+        setImage(background);
+    }
+}
